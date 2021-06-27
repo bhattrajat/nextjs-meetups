959e4a38d614cfbc0d14cb5612901066a10d34d4
Rajat Bhatt <rajatbhatt999@gmail.com>
2021-06-27 19:47:24 +0530

    Enable incremental static regeneration for homepage

--- pages/index.js
+++ pages/index.js
@@ -3,24 +3,6 @@ import MeetupList from '../components/meetups/MeetupList';
 import { connectToDatabase } from '../util/mongodb';
 import Layout from '../components/layout/Layout';
 import React from 'react';
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 5, 12345 Some City',
-    description: 'This is a first meetup!'
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Some address 10, 12345 Some City',
-    description: 'This is a second meetup!'
-  }
-];
 
 export default function Home({ meetups }) {
   return (
@@ -40,6 +22,7 @@ export async function getStaticProps(context) {
   const meetups = await db.collection('meetups').find({}).toArray();
   console.log(JSON.parse(JSON.stringify(meetups)));
   return {
-    props: { meetups: JSON.parse(JSON.stringify(meetups)) }
+    props: { meetups: JSON.parse(JSON.stringify(meetups)) },
+    revalidate: 1
   };
 }
